28b2cd8b2f93d30602cbe583800e654e62a4806e
Phadlaan <111688941+phqsuema123@users.noreply.github.com>
2025-02-02 19:02:37 +0700

    upload file

---
+++ trainning.py
@@ -0,0 +1,12 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+import tensorflow 
+from tensorsflow.keras.models import Sequentail
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.
